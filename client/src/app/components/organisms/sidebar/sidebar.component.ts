2879d719cfeac8ef7f50742e445935a2cd623c0f
yecksin <yecksin@gmail.com>
2024-04-29 21:01:33 -0500

    Side menu added

--- client/src/app/components/organisms/sidebar/sidebar.component.ts
+++ client/src/app/components/organisms/sidebar/sidebar.component.ts
@@ -1,11 +1,13 @@
 import { Component, HostBinding } from '@angular/core';
+import { IconComponent } from '../../atoms/icon/icon.component';
+import { TextComponent } from '../../atoms/text/text.component';
 
 @Component({
   selector: '[organism-sidebar]',
   standalone: true,
-  imports: [],
+  imports: [IconComponent, TextComponent],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
   @HostBinding('class.organism-sidebar') myClass = true;
