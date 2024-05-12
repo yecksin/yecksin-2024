a04aba8acaa86f8b0131d1186b71fd591896cbfc
yecksin <yecksin@gmail.com>
2024-05-11 21:13:40 -0500

    Hide sidebar if the path is service

--- client/src/app/components/organisms/sidebar/sidebar.component.ts
+++ client/src/app/components/organisms/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
-import { Component, HostBinding } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, inject, signal } from '@angular/core';
 import { IconComponent } from '../../atoms/icon/icon.component';
 import { TextComponent } from '../../atoms/text/text.component';
 import { routes } from '../../../app.routes';
 import { RoutingData } from '../../../interfaces/route-list.interface';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: '[organism-sidebar]',
@@ -11,21 +12,42 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   imports: [IconComponent, TextComponent, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarComponent {
+  router = inject(Router);
+  hideSidebar = signal(false);
+
+  private subscription: Subscription = new Subscription();
+
   @HostBinding('class.organism-sidebar') myClass = true;
   sectionList: RoutingData[] | any = [];
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-
+    this.detectRouteChange();
     this.sectionList =
       routes[0].children
         ?.filter((route) => route.data)
         .map((item) => {
           return { ...item.data, path: item.path };
         }) || [];
+  }
 
-    console.log(this.sectionList);
+  detectRouteChange() {
+    this.hideSidebarIfIsService(this.router.url);
+    this.subscription = this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.hideSidebarIfIsService(event.url);
+      });
+  }
+
+  hideSidebarIfIsService(route: string) {
+    this.hideSidebar.set(route.includes('service'));
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
